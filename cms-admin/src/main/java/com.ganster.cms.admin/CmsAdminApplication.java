e67b441594a4a3ea398228bee7987d6cc850ab60
Yoke-Yoke <717964921@qq.com>
2018-01-30 13:29:25 +0800

    thymleaf data

--- cms-admin/src/main/java/com.ganster.cms.admin/CmsAdminApplication.java
+++ cms-admin/src/main/java/com.ganster.cms.admin/CmsAdminApplication.java
@@ -1,5 +1,6 @@
 package com.ganster.cms.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Create by Yoke on 2018/1/30
  */
 @SpringBootApplication(scanBasePackages = "com.ganster.cms")
+@MapperScan("com.ganster.cms.core.dao.mapper")
 public class CmsAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(CmsAdminApplication.class, args);
--- cms-admin/src/main/java/com.ganster.cms.admin/controller/IndexController.java
+++ cms-admin/src/main/java/com.ganster.cms.admin/controller/IndexController.java
@@ -1,14 +1,18 @@
 package com.ganster.cms.admin.controller;
 
 import com.ganster.cms.admin.common.ModuleTree;
+import com.ganster.cms.core.dao.pojo.Article;
+import com.ganster.cms.core.dao.pojo.ArticleExample;
 import com.ganster.cms.core.dao.pojo.Module;
 import com.ganster.cms.core.dao.pojo.ModuleExample;
 import com.ganster.cms.core.service.ArticleService;
 import com.ganster.cms.core.service.ModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -24,8 +28,13 @@ public class IndexController {
     @Autowired
     private ModuleService moduleService;
 
-    @GetMapping("")
-    public ModelAndView index(ModelAndView modelAndView) {
+    @Autowired
+    private ArticleService articleService;
+
+    @GetMapping("index")
+    @ResponseBody
+    public List<ModuleTree> index() {
+        ModelAndView modelAndView = new ModelAndView();
         List<ModuleTree> treeList = new ArrayList<ModuleTree>();
         ModuleExample moduleExample = new ModuleExample();
         moduleExample.or().andModuleParentIdEqualTo(0);
@@ -34,10 +43,13 @@ public class IndexController {
             ModuleExample moduleExample1 = new ModuleExample();
             moduleExample1.or().andModuleParentIdEqualTo(module.getModuleId());
             List<Module> childs = moduleService.selectByExample(moduleExample1);
-            ModuleTree moduleTree = new ModuleTree(module, childs);
+            ModuleTree moduleTree = new ModuleTree();
+            moduleTree.setModule(module);
+            moduleTree.setList(childs);
             treeList.add(moduleTree);
         }
         modelAndView.addObject("treelist", treeList);
-        return modelAndView;
+        return treeList;
+//        return modelAndView;
     }
 }
