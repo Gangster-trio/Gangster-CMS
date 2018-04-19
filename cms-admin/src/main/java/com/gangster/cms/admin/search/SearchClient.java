fad5d3ac6af7dbfcd3a575f3a71281f3419bd6d6
bigmeng <947139299@qq.com>
2018-04-19 22:44:59 +0800

    [ADD] logstash sync config

--- cms-admin/src/main/java/com/gangster/cms/admin/search/SearchClient.java
+++ cms-admin/src/main/java/com/gangster/cms/admin/search/SearchClient.java
@@ -1,12 +1,23 @@
 package com.gangster.cms.admin.search;
 
+import com.gangster.cms.common.dto.SearchResult;
 import com.gangster.cms.common.pojo.Article;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("es")
 public interface SearchClient {
 
-    @PostMapping("article")
-    Article save(Article article);
+    @GetMapping("/search/article")
+    SearchResult<Article> searchArticleAllSite(@RequestParam String keyword
+            , @RequestParam(value = "page", defaultValue = "0") int page
+            , @RequestParam(value = "limit", defaultValue = "10") int limit);
+
+    @GetMapping("/search/article/{siteId}")
+    SearchResult<Article> searchArticle(@PathVariable("siteId") Integer siteId
+            , @RequestParam String keyword
+            , @RequestParam(value = "page", defaultValue = "0") int page
+            , @RequestParam(value = "limit", defaultValue = "10") int limit);
 }
