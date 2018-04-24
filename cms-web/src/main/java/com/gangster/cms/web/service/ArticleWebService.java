bff489e2ef96b53a8bd10972caf08ab6ee8b1a76
bigmeng <947139299@qq.com>
2018-04-24 21:36:39 +0800

    [UPDATE] cache, stash

---
+++ cms-web/src/main/java/com/gangster/cms/web/service/ArticleWebService.java
@@ -0,0 +1,116 @@
+package com.gangster.cms.web.service;
+
+import com.gangster.cms.common.pojo.*;
+import com.gangster.cms.dao.mapper.*;
+import com.gangster.cms.web.cache.CmsCache;
+import com.gangster.cms.web.cache.impl.HashMapCache;
+import com.gangster.cms.web.cache.impl.LRUCache;
+import com.gangster.cms.web.dto.ModelResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ArticleWebService {
+    private final Logger logger = LoggerFactory.getLogger(ArticleWebService.class);
+
+    private final
+    SiteMapper siteMapper;
+
+    private final
+    ArticleMapper articleMapper;
+
+    private final
+    CategoryMapper categoryMapper;
+
+    private final TagArticleMapper tagArticleMapper;
+
+    private final TagMapper tagMapper;
+
+    private final CategoryWebService categoryWebService;
+
+    private CmsCache<Integer, ModelResult> articleModelCache = new LRUCache<>(128);
+
+    public ArticleWebService(SiteMapper siteMapper, ArticleMapper articleMapper, CategoryMapper categoryMapper, TagArticleMapper tagArticleMapper, TagMapper tagMapper, CategoryWebService categoryWebService) {
+        this.siteMapper = siteMapper;
+        this.articleMapper = articleMapper;
+        this.categoryMapper = categoryMapper;
+        this.tagArticleMapper = tagArticleMapper;
+        this.tagMapper = tagMapper;
+        this.categoryWebService = categoryWebService;
+    }
+
+    public ModelResult getArticleModel(Integer id) {
+        if (articleModelCache.containsKey(id)) {
+            ModelResult r = articleModelCache.get(id);
+            Article a = (Article) r.get("article");
+            addArticleHit(a);
+            logger.info("get ArticleModel:{} from cache", id);
+            return r;
+        }
+
+        Article article = articleMapper.selectByPrimaryKey(id);
+        ModelResult result = new ModelResult();
+
+        if (article == null) {
+            return null;
+        }
+
+        //Get article's site
+        Site site = siteMapper.selectByPrimaryKey(article.getArticleSiteId());
+
+        //Get article's category
+        Category category = categoryMapper.selectByPrimaryKey(article.getArticleCategoryId());
+
+        //Get article's tags
+        TagArticleExample tagArticleExample = new TagArticleExample();
+        tagArticleExample.or().andArticleIdEqualTo(id);
+        List<Integer> tagIdList = tagArticleMapper.selectByExample(tagArticleExample).stream().map(TagArticle::getTagId).collect(Collectors.toList());
+        List tagList;
+        if (tagIdList.isEmpty()) {
+            tagList = Collections.EMPTY_LIST;
+        } else {
+            TagExample tagExample = new TagExample();
+            tagExample.or().andTagIdIn(tagIdList);
+            tagList = tagMapper.selectByExample(tagExample);
+        }
+        //Get 0 level categorise in this site (displayed above the homepage of the website)
+        CategoryExample categoryExample = new CategoryExample();
+        categoryExample.or().andCategorySiteIdEqualTo(article.getArticleSiteId()).andCategoryLevelEqualTo(0);
+        List<Category> categoryList = categoryMapper.selectByExample(categoryExample);
+        //Each level 0 category into category tree
+        List<CategoryTree> categoryTreeList = categoryList.stream().map(categoryWebService::toTree).collect(Collectors.toList());
+
+        result.put("category", category)
+                .put("article", article)
+                .put("tagList", tagList)
+                .put("categoryTreeList", categoryTreeList)
+                .put("site", site);
+
+        addArticleHit(article);
+
+        articleModelCache.put(id, result);
+        return result;
+    }
+
+    private void addArticleHit(Article article) {
+        //hit add
+        if (article.getArticleHit() == null) {
+            article.setArticleHit(0);
+        }
+
+        article.setArticleHit(article.getArticleHit() + 1);
+        articleMapper.updateByPrimaryKeySelective(article);
+    }
+
+    @Scheduled(fixedDelay = 1000 * 60 * 5)
+    private void flushCache() {
+        articleModelCache.clear();
+        logger.info("refresh article cache");
+    }
+}
---
+++ cms-web/src/main/java/com/gangster/cms/web/service/CategoryWebService.java
@@ -0,0 +1,120 @@
+package com.gangster.cms.web.service;
+
+import com.gangster.cms.common.pojo.*;
+import com.gangster.cms.dao.mapper.*;
+import com.gangster.cms.web.cache.CmsCache;
+import com.gangster.cms.web.cache.impl.HashMapCache;
+import com.gangster.cms.web.cache.impl.LRUCache;
+import com.gangster.cms.web.dto.ModelResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CategoryWebService {
+    private final Logger logger = LoggerFactory.getLogger(CategoryWebService.class);
+
+    private final
+    SiteMapper siteMapper;
+
+    private final
+    ArticleMapper articleMapper;
+
+    private final
+    CategoryMapper categoryMapper;
+
+    private CmsCache<Integer, ModelResult> categoryModelCache = new LRUCache<>(128);
+
+    public CategoryWebService(SiteMapper siteMapper, ArticleMapper articleMapper, CategoryMapper categoryMapper) {
+        this.siteMapper = siteMapper;
+        this.articleMapper = articleMapper;
+        this.categoryMapper = categoryMapper;
+    }
+
+
+    public ModelResult getCategoryModel(Integer id) {
+        if (categoryModelCache.containsKey(id)) {
+            ModelResult r = categoryModelCache.get(id);
+            Category c = (Category) r.get("category");
+            addCategoryHit(c);
+            logger.info("get CategoryModel:{} from cache", id);
+            return r;
+        }
+
+        ModelResult result = new ModelResult();
+
+        Category category = categoryMapper.selectByPrimaryKey(id);
+
+        if (category == null) {
+            return null;
+        }
+
+
+        //---------------------------------------default properties start----------------------------------------------//
+
+
+        Site site = siteMapper.selectByPrimaryKey(category.getCategorySiteId());
+
+        //Get 0 level categorise in this site (displayed above the homepage of the website)
+        CategoryExample categoryExample = new CategoryExample();
+        categoryExample.or().andCategorySiteIdEqualTo(category.getCategorySiteId()).andCategoryLevelEqualTo(0);
+        List<Category> categoryList = categoryMapper.selectByExample(categoryExample);
+        //Each level 0 category into category tree
+        List<CategoryTree> categoryTreeList = categoryList.stream().map(this::toTree).collect(Collectors.toList());
+
+        //Get this category's article list (without BLOBs)
+        ArticleExample articleExample = new ArticleExample();
+        articleExample.or().andArticleCategoryIdEqualTo(id);
+        List<Article> articleList = articleMapper.selectByExample(articleExample);
+
+        result.put("categoryTreeList", categoryTreeList)
+                .put("articleList", articleList)
+                .put("category", category)
+                .put("site", site);
+
+        addCategoryHit(category);
+
+        categoryModelCache.put(id, result);
+        return result;
+    }
+
+    private void addCategoryHit(Category category) {
+        Integer hit = category.getCategoryHit();
+        if (hit == null) {
+            category.setCategoryHit(0);
+            hit = 0;
+        }
+        category.setCategoryHit(hit + 1);
+        categoryMapper.updateByPrimaryKey(category);
+    }
+
+    CategoryTree toTree(Category category) {
+        CategoryTree tree = new CategoryTree();
+        int columnId = category.getCategoryId();
+        tree.setId(columnId);
+        tree.setName(category.getCategoryTitle());
+        tree.setSpread(false);
+        CategoryExample categoryExample = new CategoryExample();
+        categoryExample.or().andCategoryParentIdEqualTo(category.getCategoryId()).andCategoryStatusEqualTo(1);
+        //子栏目
+        List<Category> list = categoryMapper.selectByExample(categoryExample);
+        if (list == null || list.isEmpty()) {
+            tree.setChildren(null);
+            return tree;
+        }
+
+        tree.setChildren(list.stream().map(this::toTree).collect(Collectors.toList()));
+
+        return tree;
+    }
+
+    @Scheduled(fixedDelay = 1000 * 60 * 5)
+    private void flushCache() {
+        categoryModelCache.clear();
+        logger.info("refresh category cache");
+    }
+}
